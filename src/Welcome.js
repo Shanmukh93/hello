74eb14d19b7b31dd0bfb1098b0bd3eeda0042826
Shanmukh93 <kakarlashanmukhchowdary12@gmail.com>
2025-08-17 14:58:37 +0530

    login picture added

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,6 +5,16 @@ function Welcome() {
   const username = location.state?.username || 'User';
   return (
     <div style={{ textAlign: 'center', padding: '20px' }}>
+    <img
+        src="https://cdn-icons-png.flaticon.com/512/847/847969.png" // replace with your image
+        alt="Welcome"
+        style={{
+          width: '120px',
+          height: '120px',
+          borderRadius: '50%',
+          marginBottom: '20px',
+        }}
+      />
       <h2>Welcome ,{username}!</h2>
       <p>You have successfully logged in.</p>
     </div>
